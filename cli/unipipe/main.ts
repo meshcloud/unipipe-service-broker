dc57376db193b46c01a7ceafb36d6f0d90f13bdc
Johannes Rudolph <jrudolph@meshcloud.io>
2021-11-25 10:56:50 +0100

    fix: duplicate completions command definition

--- cli/unipipe/main.ts
+++ cli/unipipe/main.ts
@@ -1,12 +1,12 @@
-import { registerGenerateCmd } from './commands/generate.ts';
-import { registerListCmd } from './commands/list.ts';
-import { registerShowCmd } from './commands/show.ts';
-import { registerTransformCmd } from './commands/transform.ts';
-import { registerUpdateCmd } from './commands/update.ts';
-import { registerUpgradeCmd } from './commands/upgrade.ts';
+import { registerGenerateCmd } from "./commands/generate.ts";
+import { registerListCmd } from "./commands/list.ts";
+import { registerShowCmd } from "./commands/show.ts";
+import { registerTransformCmd } from "./commands/transform.ts";
+import { registerUpdateCmd } from "./commands/update.ts";
+import { registerUpgradeCmd } from "./commands/upgrade.ts";
 import { registerBrowseCmd } from "./commands/browse.ts";
-import { Command, CompletionsCommand } from './deps.ts';
-import { VERSION } from './info.ts';
+import { Command, CompletionsCommand } from "./deps.ts";
+import { VERSION } from "./info.ts";
 
 const program = new Command()
   .name("unipipe")
@@ -14,8 +14,6 @@ const program = new Command()
   .description("UniPipe CLI - supercharge your GitOps OSB service pipelines");
 
 program.command("completions", new CompletionsCommand());
-program
-  .command("completions", new CompletionsCommand());
 
 registerListCmd(program);
 registerShowCmd(program);
@@ -26,7 +24,8 @@ registerUpgradeCmd(program);
 registerBrowseCmd(program);
 
 const hasArgs = Deno.args.length > 0;
-if (hasArgs)
+if (hasArgs) {
   await program.parse(Deno.args);
-else
+} else {
   program.showHelp();
+}
