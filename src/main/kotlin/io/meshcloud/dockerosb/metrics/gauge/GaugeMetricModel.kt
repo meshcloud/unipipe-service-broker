6f8177463a567d359811b21605161c4aa39d57a6
Johannes Schwandke <jschwandke@meshcloud.io>
2021-12-23 10:35:36 +0100

    add test for reading gauge metrics from yml

--- src/main/kotlin/io/meshcloud/dockerosb/metrics/gauge/GaugeMetricModel.kt
+++ src/main/kotlin/io/meshcloud/dockerosb/metrics/gauge/GaugeMetricModel.kt
@@ -3,7 +3,7 @@ package io.meshcloud.dockerosb.metrics.gauge
 import java.math.BigDecimal
 import java.time.Instant
 
-class GaugeMetricModel(
+data class GaugeMetricModel(
     val writtenAt: Instant,
     val observedAt: Instant,
     val value: BigDecimal
