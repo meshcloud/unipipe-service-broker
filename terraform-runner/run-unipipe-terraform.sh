a2c2fd5b8874d626413ecf8b6cb2a3480f418e81
Henry Dettmer <hdettmer@meshcloud.io>
2022-11-09 12:03:25 +0100

    fix: abort cron job if git repo prep fails

--- terraform-runner/run-unipipe-terraform.sh
+++ terraform-runner/run-unipipe-terraform.sh
@@ -1,15 +1,20 @@
-#! /bin/bash
+#!/usr/bin/env bash
+# Exit on all errors and undefined vars
+set -o errexit
+set -o errtrace
+set -o pipefail
+set -o nounset
 
 REPO_NAME=instances-repo
 REPO_DIR=~/unipipe/$REPO_NAME
-if [ ! -f "$REPO_DIR" ]; then
-    cat >~/.ssh/id_rsa <<< "${GIT_SSH_KEY}"
+if [[ ! -d "$REPO_DIR" ]]; then
+    cat > ~/.ssh/id_rsa <<< "${GIT_SSH_KEY}"
     chmod 700 ~/.ssh
     chmod 600 ~/.ssh/id_rsa
     cd ~/unipipe
-    git config --global user.email $GIT_USER_EMAIL
-    git config --global user.name $GIT_USER_NAME
-    git clone $GIT_REMOTE $REPO_NAME
+    git config --global user.email "$GIT_USER_EMAIL"
+    git config --global user.name "$GIT_USER_NAME"
+    git clone "$GIT_REMOTE" "$REPO_NAME"
 fi
 
 cd $REPO_DIR
