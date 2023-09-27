76d9858714230a064ca1e1c2825ac67227e1e5d7
Henry Dettmer <hdettmer@meshcloud.io>
2023-09-27 11:09:14 +0200

    chore: update terraform-runner image

--- terraform-runner/entry.sh
+++ terraform-runner/entry.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 if [[ -z "$GIT_SSH_KEY" ]] || [[ -z "$GIT_USER_EMAIL" ]] || [[ -z "$GIT_USER_NAME" ]] || [[ -z "$GIT_REMOTE" ]] || [[ -z "$TF_VAR_platform_secret" ]]; then
   echo "Container failed to start, please provide all of the following environment variables: GIT_SSH_KEY, GIT_USER_EMAIL, GIT_USER_NAME, GIT_REMOTE, TF_VAR_platform_secret"
